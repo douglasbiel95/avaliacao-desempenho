1679c3299be2cce2175058cee7be7cd6fc6617e5
Douglas Gabriel <douglaasg11@gmail.com>
2021-10-06 16:55:49 -0300

    Projeto Criado

---
+++ src/main/java/com/douglasgabriel/avaliacaodesempenho/AvaliacaoDesempenhoApplication.java
@@ -0,0 +1,13 @@
+package com.douglasgabriel.avaliacaodesempenho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoDesempenhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoDesempenhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglasgabriel/avaliacaodesempenho/AvaliacaoDesempenhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasgabriel.avaliacaodesempenho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoDesempenhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
